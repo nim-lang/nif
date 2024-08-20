191f1af2feccbcaf43431929dec8240fa9b7e434
Andreas Rumpf <rumpf_a@web.de>
2024-08-20 16:18:37 +0200

    wip (#29)
    * wip
    
    * Update src/lib/lineinfos.nim

--- src/lib/programs.nim
+++ src/lib/programs.nim
@@ -7,22 +7,19 @@
 ## A "program" is a set of NIF files.
 
 import std / [tables, os]
-import nifreader, generictrees, hybrids, lineinfos
+import nifreader
 
 type
   DefMode = enum
     Undef, Unfinished, FromFile, FromMem
   Definition = object
     m: DefMode
-    n: LinkedNode
+    n: RestorePoint
   Module = object
     r: Reader
   Program* = object
     modules: Table[string, Module]
     syms: Table[string, Definition]
-    lits*: Literals
-    code*: Tree # concat here so that NodePos always points into this seq.
-    #dest: Tree
     thisModule: string
     nifdir: string
 
@@ -52,34 +49,24 @@ proc loadModule(p: var Program; suffix: string) =
       fatal($res & " invalid file: " & nifFile)
     p.modules[suffix] = Module(r: r)
 
-proc wasLoaded*(n: LinkedNode): bool {.inline.} =
-  result = not n.isEmpty
+proc wasLoaded*(n: RestorePoint): bool {.inline.} =
+  result = n.success
 
-proc loadSym*(p: var Program; name: string): LinkedNode =
+proc loadSym*(p: var Program; name: string): RestorePoint =
   let d = p.syms.getOrDefault(name)
   if d.m == Undef:
     let suffix = extractModuleSuffix(p, name)
     if suffix == p.thisModule:
       # no attached reader!
-      result = createEmpty()
+      result = default(RestorePoint)
     else:
       loadModule p, suffix
       let r = addr p.modules[suffix].r
+      let old = savePos(r[])
       let rp = nifreader.jumpTo(r[], name)
       if success(rp):
-        result = fromTreeAppend(p.code)
-        discard parse(r[], p.code, p.lits, NoLineInfo)
-        restore(r[], rp)
+        result = rp
+        restore(r[], old)
       else:
-        result = createEmpty()
+        result = default(RestorePoint)
       p.syms[name] = Definition(m: FromFile, n: result)
-
-proc loadHead*(p: var Program; name: string): LinkedNode =
-  # The head of a symbol is its subtree up to but excluding the "body".
-  # The body is what comes after "=":
-  # proc p(args: int): returnType = ...
-  # var x: int = ...
-  result = loadSym(p, name)
-
-proc loadBody*(p: var Program; name: string): LinkedNode =
-  result = loadSym(p, name)
