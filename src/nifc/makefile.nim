11b9a827874fc7fa08243f4d7c9554e47f83bf01
ringabout <43030857+ringabout@users.noreply.github.com>
2024-10-02 16:45:58 +0800

    refactor `c cpp n` comands, support mixing (#94)
    * refactor `c cpp n` comands, support mixing
    
    * progress:
    
    * clean up
    
    * adds ASM options

--- src/nifc/makefile.nim
+++ src/nifc/makefile.nim
@@ -1,35 +1,61 @@
-import std/[os, strformat]
+import std/[os, strformat, tables]
 import noptions
 
-proc generateMakefile*(s: State, path: string; moduleNames: seq[string]; app: string; nifcache: string; compileOption: string; destExt: string) =
+proc generateMakefileForFiles(s: State, files: seq[string],
+      makefileContent: var string, programBody: var string,
+      objectBody: var string,
+      cc: string, cflags: string, destExt: string
+    ) =
+  let nifcacheDir = s.config.nifcacheDir
+  for i in 0..<files.len:
+    let moduleNames = splitFile(files[i]).name
+    makefileContent.add " " & moduleNames & ".o"
+    programBody.add " " & nifcacheDir / moduleNames & ".o"
+    objectBody.add &"{moduleNames}.o:\n	{cc} {cflags} -c " &
+          nifcacheDir / moduleNames & fmt"{destExt} -o " &
+          nifcacheDir / moduleNames & ".o\n"
+
+proc generateMakefileForAsm(s: State, files: seq[string],
+      makefileContent: var string, programBody: var string,
+      objectBody: var string,
+      ccLinker: string) =
+  let nifcacheDir = s.config.nifcacheDir
+  const destExt = ".S"
+  for i in 0..<files.len:
+    let moduleNames = splitFile(files[i]).name
+    makefileContent.add " " & moduleNames & ".o"
+    programBody.add " " & nifcacheDir / moduleNames & ".o"
+    objectBody.add &"{moduleNames}.o:\n	{ccLinker} -c " &
+          nifcacheDir / moduleNames & fmt"{destExt} -o " &
+          nifcacheDir / moduleNames & ".o\n"
+
+proc generateMakefile*(s: State, path: string; app: string; actionTable: ActionTable) =
   var h = open(path, fmWrite)
   var makefileContent = "program:"
   var programBody = ""
   var objectBody = ""
-  let cc =
-    case s.config.backend
-    of backendC:
-      "$(CC)"
-    of backendCpp:
-      "$(CXX)"
-    else:
-      quit "unreachable"
-  let cflags =
-    case s.config.backend
-    of backendC:
-      "$(CFLAGS)"
-    of backendCpp:
-      "$(CXXFLAGS)"
-    else:
-      quit "unreachable"
-  for i in 0..<moduleNames.len:
-    makefileContent.add " " & moduleNames[i] & ".o"
-    programBody.add " " & nifcache / moduleNames[i] & ".o"
-    objectBody.add &"{moduleNames[i]}.o:\n	{cc} {cflags} -c " &
-          nifcache / moduleNames[i] & fmt"{destExt} -o " &
-          nifcache / moduleNames[i] & ".o\n"
 
-  makefileContent.add &"\n	{cc} -o " & app & programBody & "\n\n" & objectBody
+  let ccLinker = if atCpp in actionTable: "$(CXX)" else: "$(CC)"
+  for action in actionTable.keys:
+    case action
+    of atC, atCpp:
+      let (cc, cflags, destExt) = case action
+        of atC:
+          ("$(CC)", "$(CFLAGS)", ".c")
+        of atCpp:
+          ("$(CXX)", "$(CXXFLAGS)", ".cpp")
+        else:
+          quit "unreachable"
+      generateMakefileForFiles(s, actionTable[action],
+          makefileContent, programBody, objectBody,
+          cc, cflags, destExt)
+    of atNative:
+      generateMakefileForAsm(s, actionTable[action],
+          makefileContent, programBody, objectBody, ccLinker)
+    else:
+      quit "unreachable"
+
+  makefileContent.add &"\n	{ccLinker} -o " & app & programBody & "\n\n" & objectBody
 
   h.write(makefileContent)
   h.close()
