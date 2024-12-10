86fe3173824b4766275cebbe05571cee04015da5
Andreas Rumpf <rumpf_a@web.de>
2024-12-10 20:53:09 +0100

    tfib2 works (#188)

---
+++ src/nimony/tests/basics/tfib2.nim
@@ -0,0 +1,32 @@
+
+type
+  int* {.magic: Int.}
+  bool* {.magic: Bool.}
+  float* {.magic: Float.}
+
+proc `+`*(x, y: int): int {.magic: "AddI".}
+proc `-`*(x, y: int): int {.magic: "SubI".}
+
+proc `<=`*(x, y: int): bool {.magic: "LeI".}
+
+proc `+`*(x, y: float): float {.magic: "AddF64".}
+proc `-`*(x, y: float): float {.magic: "SubF64".}
+
+proc `<=`*(x, y: float): bool {.magic: "LeF64".}
+
+
+type
+  Fibable = concept
+    proc `<=`(a, b: Self): bool
+    proc `+`(x, y: Self): Self
+    proc `-`(x, y: Self): Self
+
+proc fib[T: Fibable](a: T): T =
+  if a <= 2:
+    result = 1
+  else:
+    result = fib(a-1) + fib(a-2)
+
+discard fib(8)
+
+#discard fib(8.0)
